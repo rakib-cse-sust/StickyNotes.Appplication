f91c9f22854ce961458bf97b570fb80cb8638d0f
rakib-cse-sust <rakib.cse.sust@gmail.com>
2023-04-10 01:03:57 +0100

    added api project and confugured StartupExtensions

---
+++ StickyNotes.Api/Program.cs
@@ -0,0 +1,40 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using StickyNotes.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder
+       .ConfigureServices()
+       .ConfigurePipeline();
+
+// Delete the database then run migration
+// await app.ResetDatabaseAsync();
+
+app.Run();
+
